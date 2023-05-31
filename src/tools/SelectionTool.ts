b6ed0cf5855cdc69b46af55d7757872051386c28
Leandro Del Arco <leandrodelarco@outlook.com>
2023-05-30 23:52:27 -0300

    Create Tool interface and implement SelectionTool

---
+++ src/tools/SelectionTool.ts
@@ -0,0 +1,57 @@
+import { Tile } from "../models/Tile.model";
+import { Tool } from "./Tool";
+
+export class SelectionTool implements Tool {
+
+    public name = "SELECTION";
+
+    private elementId = 'selected-tile';
+    private element: HTMLSpanElement;
+
+    private canvas: HTMLCanvasElement;
+    private tileSize: number;
+
+    public setup(canvas: HTMLCanvasElement, tileSize: number): void {
+
+        const exists = document.querySelector<HTMLSpanElement>(`#${this.elementId}`);
+
+        if (exists) {
+
+            this.element = exists
+        }
+        else {
+
+            this.element = document.createElement("span");
+            this.element.id = this.elementId;
+            this.element.style.visibility = 'hidden';
+            document.querySelector('#root')?.appendChild(this.element);
+        }
+
+        this.canvas = canvas;
+        this.tileSize = tileSize;
+        this.element.style.width = `${tileSize}px`;
+        this.element.style.height = `${tileSize}px`;
+    }
+
+    public tileMouseDown(): void {
+
+    }
+
+    public tileMouseUp(): void {
+
+    }
+
+    public tileClick(tile: Tile, button: number): void {
+
+        if (button != 0) return;
+
+        const rect = this.canvas.getBoundingClientRect();
+        this.element.style.top = `${rect.top + tile.y * this.tileSize}px`;
+        this.element.style.left = `${rect.left + tile.x * this.tileSize}px`;
+        this.element.style.visibility = 'visible';
+    }
+
+    public tileMouseMove(): void {
+        
+    }
+}
---
+++ src/tools/Tool.ts
@@ -0,0 +1,16 @@
+import { Tile } from "../models/Tile.model";
+
+export interface Tool {
+
+    name: string;
+
+    setup(canvas: HTMLCanvasElement, tileSize: number): void;
+
+    tileMouseDown(tile: Tile, button: number): void;
+
+    tileMouseUp(tile: Tile, button: number): void;
+
+    tileClick(tile: Tile, button: number): void;
+
+    tileMouseMove(tile: Tile, button: number): void;
+}
