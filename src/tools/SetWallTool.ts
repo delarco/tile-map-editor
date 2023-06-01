47943f5f25225a1f8d910a4bfdf742261260641f
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-01 00:47:11 -0300

    Implement SetWallTool

--- src/tools/SetWallTool.ts
+++ src/tools/SetWallTool.ts
@@ -5,6 +5,9 @@ export class SetWallTool implements Tool {
 
     public name = "SET WALL";
 
+    private lastTile: Tile;
+    private texture: string;
+
     public onTileSelect: (tile: Tile) => void;
     public onTileUpdate: (tile: Tile) => void;
 
@@ -12,12 +15,17 @@ export class SetWallTool implements Tool {
 
     }
 
-    public setTexture(): void {
+    public setTexture(texture: string | null): void {
 
+        if (!texture) return;
+        this.texture = texture;
     }
 
-    public tileMouseDown(): void {
+    public tileMouseDown(tile: Tile): void {
 
+        this.lastTile = tile;
+        tile.wall = this.texture;
+        this.onTileUpdate(tile);
     }
 
     public tileMouseUp(): void {
@@ -28,8 +36,13 @@ export class SetWallTool implements Tool {
 
     }
 
-    public tileMouseMove(): void {
+    public tileMouseMove(tile: Tile, button: number): void {
 
+        if (button != 1 || tile == this.lastTile) return;
+
+        this.lastTile = tile;
+        tile.wall = this.texture;
+        this.onTileUpdate(tile);
     }
 
     public canvasMouseLeave(): void {
