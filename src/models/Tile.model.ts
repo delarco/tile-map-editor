a207bcf4e96a24d1df7ef678de054a7eda67bbbf
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-01 02:19:50 -0300

    Update map and tile

--- src/models/Tile.model.ts
+++ src/models/Tile.model.ts
@@ -1,8 +1,11 @@
+import { Color } from "./Color.model";
+
 export class Tile {
 
     constructor(
         public x: number,
         public y: number,
+        public minimapColor: Color = Color.WHITE,
         public collision: boolean = false,
         public wall: string | null = null,
         public floor: string | null = null,
--- src/models/TileMap.model.ts
+++ src/models/TileMap.model.ts
@@ -1,3 +1,4 @@
+import { SpawnLocation } from "./SpawnLocation.model";
 import { Tile } from "./Tile.model";
 
 export class TileMap {
@@ -6,6 +7,10 @@ export class TileMap {
     public width: number;
     public height: number;
     public tiles: Array<Tile>;
+    public spawnLocations: Array<SpawnLocation>;
+    public skybox: string;
+    public musicList: Array<string>;
+    public sprites: Array<string>;
 
     constructor(name: string, width: number, height: number) {
 
@@ -13,10 +18,13 @@ export class TileMap {
         this.width = width;
         this.height = height;
         this.tiles = new Array<Tile>();
+        this.spawnLocations = new Array<SpawnLocation>();
+        this.musicList = new Array<string>();
+        this.sprites = new Array<string>();
 
-        for(let y = 0; y < this.height; y++) {
-            
-            for(let x = 0; x < this.width; x++) {
+        for (let y = 0; y < this.height; y++) {
+
+            for (let x = 0; x < this.width; x++) {
 
                 this.tiles.push(new Tile(x, y));
             }
