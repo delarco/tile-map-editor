4fdcfb60831c7c420bd702a6765206864be8ed2a
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-01 23:24:39 -0300

    Create and implement LayerContext

---
+++ src/context/LayerContext.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Layer } from "../components/Layers/LayersComponent";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export type LayerContextType = {
+  layer: Layer | null;
+  selectLayer: (layer: Layer) => void;
+};
+
+export const LayerContext = React.createContext<LayerContextType | null>(null);
+
+export const LayerProvider: React.FC<Props> = ({ children }) => {
+
+  const [selectedLayer, setSelectedLayer] = React.useState<Layer | null>(null);
+
+  const selectLayer = (layer: Layer) => {
+
+    setSelectedLayer(layer);
+  }
+
+  return (
+    <LayerContext.Provider value={{ layer: selectedLayer, selectLayer }}>
+        selected layer: {selectedLayer}
+        {children}
+    </LayerContext.Provider>
+  );
+};
