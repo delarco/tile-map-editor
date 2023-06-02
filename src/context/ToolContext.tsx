cce413e53fee8283390d234116aa7974b101f9cb
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-01 23:24:52 -0300

    Create and implement ToolContext

---
+++ src/context/ToolContext.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Tool } from "../tools/Tool";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export type ToolContextType = {
+  tool: Tool | null;
+  selectTool: (tool: Tool) => void;
+};
+
+export const ToolContext = React.createContext<ToolContextType | null>(null);
+
+export const ToolProvider: React.FC<Props> = ({ children }) => {
+
+  const [selectedTool, setSelectedTool] = React.useState<Tool | null>(null);
+
+  const selectTool = (tool: Tool) => {
+
+    setSelectedTool(tool);
+  }
+
+  return (
+    <ToolContext.Provider value={{ tool: selectedTool, selectTool }}>
+        selected tool: {selectedTool?.name}
+        {children}
+    </ToolContext.Provider>
+  );
+};
