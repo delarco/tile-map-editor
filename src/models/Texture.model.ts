4eb667ca55fa981b959f1c49cb60b5d04a44c7b4
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-02 23:01:32 -0300

    Create and implement Texture model

---
+++ src/models/Texture.model.ts
@@ -0,0 +1,34 @@
+import React, { HTMLAttributes } from "react";
+
+export class Texture {
+
+    public filename: string;
+    public description: string;
+    public keywords: Array<string>;
+    public width: number;
+    public height: number;
+    public image: React.DetailedReactHTMLElement<HTMLAttributes<HTMLImageElement>, HTMLImageElement>;
+
+    constructor(filename: string) {
+
+        this.filename = filename;
+
+        this.image = React.createElement(
+            "img",
+            {
+                //id: ``;
+                src: `/assets/textures/${filename}`,
+                style: {
+                    width: '100%',
+                    height: '100%',
+                },
+                onLoad: (ev) => {
+
+                    const imageElement = <HTMLImageElement>ev.target;
+                    this.width = imageElement.naturalWidth;
+                    this.height = imageElement.naturalHeight;
+                }
+            },
+        );
+    }
+}
